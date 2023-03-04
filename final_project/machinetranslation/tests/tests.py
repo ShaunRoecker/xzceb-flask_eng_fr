ec2998ce456f4830911c22bb9d2acb0297c7f925
s-roecker <94014218+s-roecker@users.noreply.github.com>
2023-03-04 11:44:00 -0500

    Final changes

---
+++ final_project/machinetranslation/tests/tests.py
@@ -0,0 +1,21 @@
+# import unittest
+# from translator import *
+
+# class TestTranslator(unittest.TestCase):
+    
+#     def test_french_to_english(self):
+#         a = translator.frenchToEnglish("Bonjour")
+#         b = "Hello"
+#         self.assertEqual(a, b)
+#         self.assertIsNotNone(a)
+
+#     def test_english_to_french(self):
+#         a = translator.englishToFrench("Hello")
+#         b = "Bonjour"
+#         self.assertEqual(a, b)
+#         self.assertIsNotNone(a)
+
+# if __name__ == '__main__':
+#     unittest.main()
+
+
